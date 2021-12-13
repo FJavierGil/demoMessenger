5236400a0d016bbddfb075eece7f79f4bebef043
F. Javier Gil <franciscojavier.gil@upm.es>
2021-12-13 14:23:59 +0100

    Actualización de estilos/LF

--- .docker/php/entrypoint.sh
+++ .docker/php/entrypoint.sh
@@ -7,9 +7,9 @@ chown -hR dev:dev /home/wwwroot/aos/
 # chown -hR www-data:www-data /home/wwwroot/aos/var/
 
 # set permissions for future files and folders
-setfacl -dR -m u:"www-data":rwX -m u:$(whoami):rwX /home/wwwroot/aos/var/
+setfacl -dR -m u:"www-data":rwX -m u:"$(whoami)":rwX /home/wwwroot/aos/var/
 
 # set permissions on the existing files and folders
-setfacl -R -m u:"www-data":rwX -m u:$(whoami):rwX /home/wwwroot/aos/var/
+setfacl -R -m u:"www-data":rwX -m u:"$(whoami)":rwX /home/wwwroot/aos/var/
 
 exec "$@"
